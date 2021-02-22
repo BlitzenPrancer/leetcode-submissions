15de7e46f4d7ff1e648575b609ce68d44fedb41f
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-02-22 20:51:04 +0530

    Create optimal.go

---
+++ Linked List/61. Rotate List/optimal.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+    var curr *ListNode
+    curr = head
+    length := 1
+    for curr.Next != nil {
+        length++
+        curr = curr.Next
+    }
+    curr.Next = head
+    // If K is greter then length of the list then reduce K
+    if (k >= length) {
+    k = k % length
+    }
+    k = length - k
+    for(k > 0) {
+        curr = curr.Next
+        k--
+    }
+    head = curr.Next
+    curr.Next = nil
+    return head
+}
